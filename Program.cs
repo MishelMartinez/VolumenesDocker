57374c101a54a5db6c1db310bbfab7f9af55b626
MishelMartinez <lilian.mishel06@gmail.com>
2022-10-24 13:35:02 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Text;
+// See https://aka.ms/new-console-template for more information
+
+
+
+string directory = Environment.GetEnvironmentVariable("DIRECTORY");
+string fileName = Environment.GetEnvironmentVariable("FILENAME");
+
+if (Directory.Exists(directory))
+{
+
+    StringBuilder builder = new StringBuilder();
+
+    for (int i = 0; i < 10; i++)
+    {
+        builder.AppendLine($"Linea de prueba {i + 1}");
+    }
+
+    File.WriteAllText(Path.Combine(directory, fileName), builder.ToString());
+}
+else
+{
+    Console.WriteLine("Ya trono como cohete");
+}
+
+
+
+Console.WriteLine($"El directorio donde se almacenara es: {directory}");
+Console.WriteLine($"El Nombre del archivo es: {fileName}");
+System.Console.Read();
